7b7fcb3888827f6d7d7238b098d91aacc72b3f5f
Rodion Sibov <rodionsibov@gmail.com>
2021-05-01 04:13:57 +0300

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div
+      v-if="user"
+      class="w-3/12 m-auto bg-purple-100 mt-4 pb-10 shadow-2xl flex justify-center flex-col items-center rounded-lg border-2 border-purple-300"
+    >
+      <div class="flex justify-center m-4">
+        <img :src="user.avatar" alt="Avatar" />
+      </div>
+      <h3 class="text-lg text-purple-900 uppercase text-center font-bold">
+        {{ user.first_name }}
+        {{ user.last_name }}
+      </h3>
+      <h5 class="text-gray-700">
+        {{ user.email }}
+      </h5>
+    </div>
   </div>
 </template>
+
+<script>
+import { useRoute } from "vue-router";
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const route = useRoute();
+    const state = reactive({
+      user: null,
+    });
+
+    fetch(`https://reqres.in/api/users/${route.params.slug}`)
+      .then((res) => res.json())
+      .then(({ data }) => {
+        console.log(data);
+        state.user = data;
+      });
+
+    return { ...toRefs(state) };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full flex justify-center">
+    <input
+      v-model="text"
+      class="p-2 border-purple-900 border-2 rounded"
+      type="text"
+      placeholder="Enter User here"
+    />
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div
+      class="ml-4 text-2xl text-purple-900"
+      v-for="(user, index) in filteredUser"
+      :key="index"
+    >
+      <router-link :to="`/about/${urlIdLookup[user.first_name]}`">
+        {{ user.first_name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed, reactive, toRefs } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    const state = reactive({
+      users: [],
+      urlIdLookup: {},
+      text: "",
+      filteredUser: computed(() => updateUser()),
+    });
+
+    function updateUser() {
+      if (!state.text) {
+        return [];
+      }
+      return state.users.filter((user) => user.first_name.includes(state.text));
+    }
+
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then(({ data }) => {
+        console.log(data);
+        state.users = data;
+        state.urlIdLookup = data.reduce(
+          (acc, cur, index) => (acc = { ...acc, [cur.first_name]: index + 1 }),
+          {}
+        );
+      });
+
+    return { ...toRefs(state) };
+  },
+};
 </script>
